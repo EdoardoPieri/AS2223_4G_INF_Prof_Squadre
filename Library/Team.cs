045b81cd10961bbb242d869538eb67065fc007c2
Edoardo Pieri <edoardopieri05@libero.it>
2023-01-16 10:20:37 +0100

    feat: aggiunta costruttore Team

--- Library/Team.cs
+++ Library/Team.cs
@@ -28,7 +28,8 @@ namespace Library
 
         public Team(string name)
         {
-            
+            this.name = name;
+            players = new List<Player>();
         }
 
         /// <summary>
